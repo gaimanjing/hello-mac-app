b7fd1d993826589f84e77a1ddd0480706fe4d4f3
gaimanjing <gaimanjing@gmail.com>
2020-04-29 22:28:59 +0800

    1

---
+++ hello-mac-app/AppDelegate.swift
@@ -0,0 +1,25 @@
+//  AppDelegate.swift
+//  hello-mac-app
+//
+//  Created by gaimanjing on 2020/4/29.
+//  Copyright © 2020 gaimanjing. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ hello-mac-app/ViewController.swift
@@ -0,0 +1,26 @@
+//  ViewController.swift
+//  hello-mac-app
+//
+//  Created by gaimanjing on 2020/4/29.
+//  Copyright © 2020 gaimanjing. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
